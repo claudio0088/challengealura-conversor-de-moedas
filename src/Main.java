2678fc083fba1ed1b45d276f7b0f7102bd77bf7d
claudio0088 <claudiogabrielsilva08@gmail.com>
2024-12-08 19:09:24 -0300

    finalizando os métodos

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Conversor conversor = new Conversor();
         Moedas novasMoedas = conversor.converterMoedas();
 
-        System.out.println(novasMoedas.converterDolarParaReal(40));
+        System.out.println(novasMoedas.converteDolarParaReal(40));
         System.out.println(novasMoedas);
     }
 }
--- src/Moedas.java
+++ src/Moedas.java
@@ -2,10 +2,29 @@ public record Moedas(String baseCode,double ars,double bob,
                      double brl, double cop
         ,double usd) {
 
-    public double converterDolarParaReal(double valor){
+    public double converteDolarParaReal(double valor){
         return  valor * brl;
-
     }
+    public double converteRealParaDolar(double valor){
+        return valor / brl;
+    }
+
+    public double converteDolarParaPesoArgentino(double valor){
+        return valor * ars;
+    }
+    public double convertePesoArgentinoparaDolar(double valor){
+       return valor / ars;
+    }
+
+    public double converteDolarParaPesoColombiano(double valor){
+        return valor * cop;
+    }
+    public double convertePesoColombianoParaDolar(double valor){
+        return valor / cop;
+    }
+
+
+
 
 }
 
